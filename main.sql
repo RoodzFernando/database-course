95173affc8ef019b80f3d5d7c00e070fd65b22a7
RoodzFernando <fleurantrfernando@gmail.com>
2020-01-07 12:56:36 -0500

    Finished More JOIN operations

--- main.sql
+++ main.sql
@@ -386,6 +386,115 @@ Answer: SELECT DISTINCT mdate, team1,
         GROUP BY mdate,team1,team2
         ORDER BY mdate,matchid,team1,team2
 
+More JOIN operations
+
+1. List the films where the yr is 1962 [Show id, title]
+
+Answer: SELECT id, title FROM movie
+        WHERE yr = 1962
+
+2. Give year of 'Citizen Kane'.
+
+Answer: SELECT yr FROM movie
+        WHERE title = 'Citizen Kane'
+
+3. List all of the Star Trek movies, include the id, title and yr (all of these movies include the words Star Trek in the title). Order results by year.
+
+Answer: SELECT id, title, yr FROM movie
+        WHERE title LIKE 'Star Trek%' 
+        ORDER BY yr
+
+4. What id number does the actor 'Glenn Close' have?
+
+Answer: SELECT id FROM actor
+        WHERE name = 'Glenn Close'
+
+5. What is the id of the film 'Casablanca'
+
+Answer: SELECT id FROM movie
+        WHERE title = 'Casablanca'
+
+6. Obtain the cast list for 'Casablanca'.
+what is a cast list?
+Use movieid=11768, (or whatever value you got from the previous question)
+
+Answer: SELECT name FROM actor
+        JOIN casting ON (id = actorid)
+        WHERE movieid = 11768
+
+7. Obtain the cast list for the film 'Alien'
+
+Answer: SELECT name FROM actor
+        JOIN casting ON (actor.id = actorid)
+        JOIN movie ON (movie.id = movieid)
+        WHERE title = 'Alien'
+
+8. List the films in which 'Harrison Ford' has appeared
+
+Answer: SELECT title FROM casting 
+        JOIN movie ON (id = movieid)
+        WHERE actorid = 2216
+
+9. List the films where 'Harrison Ford' has appeared - but not in the starring role. [Note: the ord field of casting gives the position of the actor. If ord=1 then this actor is in the starring role]
+
+Answer: SELECT title FROM casting 
+        JOIN movie ON (id = movieid)
+        WHERE actorid = 2216 AND ord <> 1
+
+10. List the films together with the leading star for all 1962 films.
+
+Answer: SELECT title, name FROM movie
+        JOIN casting ON (movie.id = movieid)
+        JOIN actor ON (actor.id = actorid)
+        WHERE yr = 1962 AND ord = 1
+
+11. Which were the busiest years for 'Rock Hudson', show the year and the number of movies he made each year for any year in which he made more than 2 movies.
+
+Answer: SELECT yr,COUNT(title) FROM movie
+        JOIN casting ON movie.id=movieid
+        JOIN actor   ON actorid=actor.id
+        WHERE name='Rock Hudson'
+        GROUP BY yr
+        HAVING COUNT(title) > 2
+
+12. List the film title and the leading actor for all of the films 'Julie Andrews' played in.
+
+Answer: SELECT title, name FROM movie
+        JOIN casting ON (movieid = movie.id AND ord = 1)
+        JOIN actor ON (actor.id = actorid)
+        WHERE movie.id IN (
+        SELECT movieid FROM casting
+        WHERE actorid IN (SELECT id FROM actor WHERE name = 'Julie Andrews'))
+
+13. Obtain a list, in alphabetical order, of actors who've had at least 30 starring roles.'
+
+Answer: SELECT name FROM actor
+        JOIN casting ON (actor.id = actorid)
+        WHERE  ord=1 GROUP BY name
+        HAVING COUNT(movieid)>=30
+
+14. List the films released in the year 1978 ordered by the number of actors in the cast, then by title.
+
+Answer: SELECT movie.title,COUNT(casting.actorid) FROM movie 
+        INNER JOIN casting ON movie.id=casting.movieid 
+        INNER JOIN actor ON casting.actorid=actor.id
+        WHERE yr=1978
+        group by title
+        ORDER BY COUNT(actorid) DESC, title
+
+15. List all the people who have worked with 'Art Garfunkel'.
+
+Answer: SELECT actor.name FROM actor 
+        INNER JOIN casting ON actor.id=casting.actorid
+        INNER JOIN movie ON casting.movieid=movie.id
+        WHERE casting.movieid IN 
+        (SELECT movie.id FROM movie
+        INNER JOIN casting ON casting.movieid=movie.id
+        INNER JOIN actor ON actor.id=casting.actorid
+        WHERE actor.name='Art Garfunkel')
+        AND actor.name<>'Art Garfunkel'
+
+
     
 
 
